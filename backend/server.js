ed6d562128d47825039109176c541b2ab871590c
brandon-whitman-orlin <brandonwhitmanorlin@gmail.com>
2025-01-20 16:16:53 -0500

    Changing API Key Handling

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ const app = express();
 const port = process.env.PORT || 5000; // Use the port Render provides or fallback to 5000
 
 // Environment variables for API keys
-const eiaApiKey = process.env.EIA_API_KEY;
-const fredApiKey = process.env.FRED_API_KEY;
+// const eiaApiKey = process.env.EIA_API_KEY;
+// const fredApiKey = process.env.FRED_API_KEY;
 
 // Middleware
 app.use(cors()); // Enable CORS to allow requests from your frontend
@@ -21,7 +21,7 @@ app.get('/api/gas', async (req, res) => {
     try {
         const response = await axios.get('https://api.eia.gov/v2/petroleum/pri/gnd/data/', {
             params: {
-                api_key: eiaApiKey,
+                api_key: EIA_API_KEY,
                 frequency: 'weekly',
                 'data[0]': 'value',
                 'facets[duoarea][]': 'NUS',
@@ -55,7 +55,7 @@ app.get('/api/egg', async (req, res) => {
         const response = await axios.get('https://api.stlouisfed.org/fred/series/observations', {
             params: {
                 series_id: 'APU0000708111',
-                api_key: fredApiKey,
+                api_key: FRED_API_KEY,
                 file_type: 'json',
                 limit: 5,
                 sort_order: 'desc',
